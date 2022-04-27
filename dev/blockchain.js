6eca4786c6c4c1564e4f60878e7b6aba26101a0b
vladyslovestepanov <vstepanov@neonomics.io>
2022-04-27 14:13:32 +0300

    Blockchain Constructor Function

---
+++ dev/blockchain.js
@@ -0,0 +1,14 @@
+function Blockchain() {
+    this.chain = [];
+    this.newTransactions = [];
+}
+
+// // it could be implemented with a class 
+// class Blockchain {
+//     constructor() {
+//         this.chain = [];
+//         this.newTransactions = [];
+//     } 
+
+//     // 
+// }
